ec67bc49c68c349057aeff8f0df7a545ef8f64df
Nicola Mastrandrea <nick88msn@gmail.com>
2020-11-30 21:04:26 +0100

    Update script with filename cleaning

--- script.sh
+++ script.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+# Clean filenames using rename (sudo apt install rename)
+find . | rename 's/ /_/g'
+find . | rename 's/-//g'
+find . | rename 's/__/_/g'
+
+# Get filename from .pdf.p7m files
+# Strip Header and Footer from .p7m file and write the output inside a .pdf file
 for f in *.p7m  
 do 
     filename=$(basename $f .p7m)
